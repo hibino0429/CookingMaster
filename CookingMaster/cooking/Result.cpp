8b5f0619562bd93b1191fe8823435644cc2b69f5
jiyoung-m <wud192493@gmail.com>
2018-02-02 00:32:48 +0900

    リザルト画面

--- CookingMaster/cooking/Result.cpp
+++ CookingMaster/cooking/Result.cpp
@@ -6,6 +6,8 @@ bool Result::Initialize()
 	a = 0;
 	flag = false;
 	timeCnt = 0;
+	foodNum = judgeScore();
+
 	bghandle = LoadGraph("./Graph/result.png");
 	sound.SetBGM("./Sound/grill.ogg");
 	Rcarrot = LoadGraph("./Graph/Rcarrot.png");
@@ -16,7 +18,21 @@ bool Result::Initialize()
 	Rbrory = LoadGraph("./Graph/Rbrory.png");
 	Ronion = LoadGraph("./Graph/Ronion.png");
 
+	LoadDivGraph("./Graph/effusion1.png", 3, 3, 1, 469,281, eff[0].pic);
+	LoadDivGraph("./Graph/effusion2.png", 3, 3, 1, 409, 246, eff[1].pic);
+	eff[0].init.initialize_E(236, 358, 300);
+	eff[1].init.initialize_E(730, 380, 350);
+
+	LoadDivGraph("./Graph/score.png", 3,1,3, 598, 203, score_pic);
+	score_scale = 5.0f;
+
+
 	pos_initilize();
+	for (int i = 0; i < 2; i++)
+	{
+		eff[i].aniCnt = 0.0f;
+		eff[i].flag = false;
+	}
 
 	if (bghandle == -1)
 	{
@@ -24,7 +40,6 @@ bool Result::Initialize()
 	}
 	return true;
 }
-
 
 void Result::Update()
 {
@@ -39,7 +54,8 @@ void Result::Update()
 	{
 		
 	}
-	for (int i = 0; i < scoreAni(); ++i)
+
+	for (int i = 0; i < foodNum; ++i)
 	{
 		TimeAni(food[i], i);	//一定時間がなったかチェック
 		if (food[i].flag == true)
@@ -48,12 +64,20 @@ void Result::Update()
 		}
 	}
 
-	if (Key(KEY_INPUT_Z) == 1)
+	for (int i = 0; i < 2; ++i)
 	{
-		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);		//ブレンドモードをオフ
-		SceneManeger::GetInstance()->ChangeScene(new Title);
-		Score() = 0;
+		AppearEffusion(eff[i]);
 	}
+
+	if (timeCnt >= (foodNum * 10) + 400) {
+		if (Key(KEY_INPUT_Z) == 1)
+		{
+			SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);		//ブレンドモードをオフ
+			SceneManeger::GetInstance()->ChangeScene(new Title);
+			Score() = 0;
+		}
+	}
+
 }
 
 void Result::Draw()
@@ -62,25 +86,22 @@ void Result::Draw()
 	SetDrawBlendMode(DX_BLENDMODE_ALPHA, a);		//ブレンドモードαを設定
 	DrawGraph(0, 0, bghandle, true);
 
-	for (int i = 0; i < scoreAni(); ++i)
+	for (int i = 0; i < foodNum; ++i)
 	{
-		if (food[i].flag == true)
-			switch (food[i].pos.pic)
-			{
-			case 0:  DrawRotaGraphF(food[i].pos.x, food[i].pos.y, 1.0, food[i].pos.rotate, Rcabbage, true); break;
-			case 1:  DrawRotaGraphF(food[i].pos.x, food[i].pos.y, 1.0, food[i].pos.rotate, Rpotato, true); break;
-			case 2:  DrawRotaGraphF(food[i].pos.x, food[i].pos.y, 1.0, food[i].pos.rotate, Rtomato, true); break;
-			case 3:  DrawRotaGraphF(food[i].pos.x, food[i].pos.y, 1.0, food[i].pos.rotate, Rbrory, true); break;
-			case 4:  DrawRotaGraphF(food[i].pos.x, food[i].pos.y, 1.0, food[i].pos.rotate, Rcarrot, true); break;
-			case 5:  DrawRotaGraphF(food[i].pos.x, food[i].pos.y, 1.0, food[i].pos.rotate, Ronion, true); break;
-			case 6:  DrawRotaGraphF(food[i].pos.x, food[i].pos.y, 1.0, food[i].pos.rotate, Rtomato2, true); break;
-			}
+		DrawFood(food[i]);
+	}
+	
+	DrawScore();
+	
+	for (int i = 0; i < 2; ++i)
+	{
+		DrawEffusion(eff[i]);
 	}
 }
 
 void Result::Finalize()
 {
-	for (int i = 0; i < scoreAni(); i++)
+	for (int i = 0; i < foodNum; i++)
 	{
 		DeleteGraph(food[i].pic);
 	}
@@ -91,46 +112,60 @@ void Result::Finalize()
 void Result::pos_initilize()
 {
 	//x, y, 到着座標(y)、画像の回転、画像の番号
-	food[0].pos.SetPOS_R(438, -150, 118, 0.0, 0);
-	food[1].pos.SetPOS_R(437, -150, 430, PI*1.2, 0);
-	food[2].pos.SetPOS_R(400, -150, 200, PI/6,3);
-	food[3].pos.SetPOS_R(600, -150, 300, PI/3,0);
-	food[4].pos.SetPOS_R(580, -150, 230, PI*1.5, 2);
-	food[5].pos.SetPOS_R(600, -150, 150, PI/2, 4);
-	food[6].pos.SetPOS_R(320, -150, 220, 0.0, 1);
-	food[7].pos.SetPOS_R(520, -150, 310, PI/4, 5);
-	food[8].pos.SetPOS_R(550, -150, 360, PI*1.4 , 0);
-	food[9].pos.SetPOS_R(337, -150, 305, PI*1.4, 6);
-	food[10].pos.SetPOS_R(392, -150, 362, PI*1.6, 3);
-	food[11].pos.SetPOS_R(607, -150, 267, PI*1.2, 5);
-	food[12].pos.SetPOS_R(453, -150, 295, PI*1.5, 4);
-	food[13].pos.SetPOS_R(495, -150, 225, PI*1.2, 0);
-	food[14].pos.SetPOS_R(558, -150, 440, PI / 3, 2);
-	food[15].pos.SetPOS_R(542, -150, 106, 0.0, 1);
-	food[16].pos.SetPOS_R(314, -150, 200, PI /4*3, 0);
-	food[17].pos.SetPOS_R(510, -150, 150, PI*1.8, 6);
-	food[18].pos.SetPOS_R(612, -150, 342, PI / 4, 3);
-	food[19].pos.SetPOS_R(431, -150, 454, 0.0, 4);
-	food[20].pos.SetPOS_R(357, -150, 237, 0.0, 5);
-	food[21].pos.SetPOS_R(583, -150, 407, PI*0.7, 0);
-	food[22].pos.SetPOS_R(356, -150, 141, 0.0, 4);
-	food[23].pos.SetPOS_R(525, -150, 422, 0.0, 3);
-	food[24].pos.SetPOS_R(637, -150, 197, 0.0, 1);
-	food[25].pos.SetPOS_R(646, -150, 269, 0.0, 4);
-	food[26].pos.SetPOS_R(583, -150, 235, PI/6, 3);
-	food[27].pos.SetPOS_R(425, -150, 323, PI *1.8, 0);
-	food[28].pos.SetPOS_R(401, -150, 260, PI *1.8, 3);
-	food[29].pos.SetPOS_R(618, -150, 382, 0.0, 1);
-	food[30].pos.SetPOS_R(541, -150, 335, 0.0, 5);
-	food[31].pos.SetPOS_R(446, -150, 335, PI *1.5, 2);
-	food[32].pos.SetPOS_R(447, -150, 149, 0.0, 1);
+	food[0].pos.SetPOS_R(438, 0, 118, 0.0, 0);
+	food[1].pos.SetPOS_R(437, 0, 430, PI*1.2, 0);
+	food[2].pos.SetPOS_R(400, 0, 200, PI/6,3);
+	food[3].pos.SetPOS_R(600, 0, 300, PI/3,0);
+	food[4].pos.SetPOS_R(580, 0, 230, PI*1.5, 2);
+	food[5].pos.SetPOS_R(600, 0, 150, PI/2, 4);
+	food[6].pos.SetPOS_R(320, 0, 220, 0.0, 1);
+	food[7].pos.SetPOS_R(520, 0, 310, PI/4, 5);
+	food[8].pos.SetPOS_R(550, 0, 360, PI*1.4 , 0);
+	food[9].pos.SetPOS_R(337, 0, 305, PI*1.4, 6);
+	food[10].pos.SetPOS_R(392, 0, 362, PI*1.6, 3);
+	food[11].pos.SetPOS_R(607, 0, 267, PI*1.2, 5);
+	food[12].pos.SetPOS_R(453, 0, 295, PI*1.5, 4);
+	food[13].pos.SetPOS_R(495, 0, 225, PI*1.2, 0);
+	food[14].pos.SetPOS_R(558, 0, 440, PI / 3, 2);
+	food[15].pos.SetPOS_R(542, 0, 106, 0.0, 1);
+	food[16].pos.SetPOS_R(314, 0, 200, PI /4*3, 0);
+	food[17].pos.SetPOS_R(510, 0, 150, PI*1.8, 6);
+	food[18].pos.SetPOS_R(612, 0, 342, PI / 4, 3);
+	food[19].pos.SetPOS_R(431, 0, 454, 0.0, 4);
+	food[20].pos.SetPOS_R(357, 0, 237, 0.0, 5);
+	food[21].pos.SetPOS_R(583, 0, 407, PI*0.7, 0);
+	food[22].pos.SetPOS_R(356, 0, 141, 0.0, 4);
+	food[23].pos.SetPOS_R(525, 0, 422, 0.0, 3);
+	food[24].pos.SetPOS_R(637, 0, 197, 0.0, 1);
+	food[25].pos.SetPOS_R(646, 0, 269, 0.0, 4);
+	food[26].pos.SetPOS_R(583, 0, 235, PI/6, 3);
+	food[27].pos.SetPOS_R(425, 0, 323, PI *1.8, 0);
+	food[28].pos.SetPOS_R(401, 0, 260, PI *1.8, 3);
+	food[29].pos.SetPOS_R(618, 0, 382, 0.0, 1);
+	food[30].pos.SetPOS_R(541, 0, 335, 0.0, 5);
+	food[31].pos.SetPOS_R(446, 0, 335, PI *1.5, 2);
+	food[32].pos.SetPOS_R(447, 0, 149, 0.0, 1);
+}
+int Result::judgeScore()
+{
+	if (Score() >= 100 && Score() < 200)		//スコアを受けて
+	{
+		eff_score = Good;
+		return 21;								//表す画像の個数を返す
+	}
+	if (Score() >= 200)
+	{
+		eff_score = Great;
+		return 33;
+	}
+	eff_score = Bad;
+	return 10;
 }
 
 void Result::PutAni(Food& f) 
 {
 	f.pos.y = f.anim.BounceOut(f.anim.Time(10), 0, f.pos.arrival - 0, 10.f);   //easingアニメーション
 }
-
 void Result::TimeAni(Food& f, int n)
 {
 	if (timeCnt % ((10*n)+100) == 0) //（タイムカウント％（フレーム数＊順番））＋ディレイ
@@ -139,15 +174,54 @@ void Result::TimeAni(Food& f, int n)
 	}
 }
 
-int Result::scoreAni()
+void Result::DrawFood(Food& f)
 {
-	if (Score() >= 100 && Score() < 200)		//スコアを受けて
-	{
-		return 21;								//表す画像の個数を返す
+	if (f.flag == true)
+		switch (f.pos.pic)
+		{
+		case 0:  DrawRotaGraphF(f.pos.x, f.pos.y, 1.0, f.pos.rotate, Rcabbage, true); break;
+		case 1:  DrawRotaGraphF(f.pos.x, f.pos.y, 1.0, f.pos.rotate, Rpotato, true); break;
+		case 2:  DrawRotaGraphF(f.pos.x, f.pos.y, 1.0, f.pos.rotate, Rtomato, true); break;
+		case 3:  DrawRotaGraphF(f.pos.x, f.pos.y, 1.0, f.pos.rotate, Rbrory, true); break;
+		case 4:  DrawRotaGraphF(f.pos.x, f.pos.y, 1.0, f.pos.rotate, Rcarrot, true); break;
+		case 5:  DrawRotaGraphF(f.pos.x, f.pos.y, 1.0, f.pos.rotate, Ronion, true); break;
+		case 6:  DrawRotaGraphF(f.pos.x, f.pos.y, 1.0, f.pos.rotate, Rtomato2, true); break;
+		}
+}
+void Result::DrawScore()
+{
+	if (timeCnt >= (foodNum * 10) + 200) {
+		if (score_scale >= 1.0f)
+		{
+			score_scale -= 0.1f;
+		}
+		switch (eff_score)
+		{
+		case 0:  DrawRotaGraph(480, 150, score_scale, 0.0, score_pic[eff_score], true); break; //Bad
+		case 1:  DrawRotaGraph(480, 150, score_scale, 0.0, score_pic[eff_score], true); break; //Good
+		case 2:  DrawRotaGraph(480, 150, score_scale, 0.0, score_pic[eff_score], true); break; //Great
+		}
+
 	}
-	if (Score() >= 200)
-	{
-		return 33;
+}
+void Result::AppearEffusion(Effusion& e)
+{
+	if (timeCnt >= (foodNum * 10) + e.init.appear) {
+		e.flag = true;
 	}
-	return 10;
+}
+void Result::DrawEffusion(Effusion& e)
+{
+	if (e.flag == true) {
+		if (e.aniCnt <= 1.0f) {
+			e.aniCnt += 0.1f;
+		}
+		switch (eff_score)
+		{
+		case 0:  DrawRotaGraph(e.init.x, e.init.y, e.aniCnt, 0.0, e.pic[0], true); break; //Bad
+		case 1:  DrawRotaGraph(e.init.x, e.init.y, e.aniCnt, 0.0, e.pic[1], true); break; //Good
+		case 2:  DrawRotaGraph(e.init.x, e.init.y, e.aniCnt, 0.0, e.pic[2], true); break; //Great
+		}
+		
+	}
 }
--- CookingMaster/cooking/Result.h
+++ CookingMaster/cooking/Result.h
@@ -7,6 +7,7 @@ public:
 	void Update()	  override;
 	void Draw()       override;
 	void Finalize()   override;
+
 	struct POS_R
 	{
 		void SetPOS_R(float x, float y, float arrival, double rotate, int pic)
@@ -19,12 +20,28 @@ public:
 		}
 		float x, y, arrival; double rotate; int pic;
 	};
+	struct INIT_E
+	{
+		void initialize_E(int x, int y, int appear)
+		{
+			this->x = x;
+			this->y = y;
+			this->appear = appear;
+		}
+		int x, y, appear;
+	};
+
 	int a;
 	Sound sound;
 
 
-
 	int timeCnt;
+	int foodNum;
+	enum Eff_Score
+	{
+		Bad, Good, Great,
+	};
+	Eff_Score eff_score;
 
 	struct Food
 	{
@@ -33,17 +50,37 @@ public:
 		bool flag;
 		POS_R pos;
 	};
-	int Rcabbage, Rpotato, Rtomato, Rbrory, Rcarrot, Ronion, Rtomato2;
+	int Rcabbage, Rpotato, Rtomato, Rbrory, Rcarrot, Ronion, Rtomato2;	//‰æ‘œ
+
+
+	struct Effusion
+	{
+		int bg;
+		bool flag;
+		float aniCnt;
+		INIT_E init;
+		int pic[3];
+	};
 
 	Food food[33];
+	Effusion eff[2];
+
+	int score_pic[3];
+	float score_scale;
+
 
 	void pos_initilize();
 	void PutAni(Food& f);
 	void TimeAni(Food& f, int n);
-	int scoreAni();
+	int judgeScore();
+
+	void DrawFood(Food& f);
+	void DrawScore();
+	void AppearEffusion(Effusion& e);
+	void DrawEffusion(Effusion& e);
+
 
 private:
 	int bghandle;
-
 	bool flag;
 };
